f6f757008b76cd98f965fa05acdd3993e486c817
Raymond-Yang-2001 <2723701951@qq.com>
2022-12-17 16:17:17 +0800

    Forward Prediction(Not implementation of F1 Precision ReCall)

--- NueralNeywork/Network.py
+++ NueralNeywork/Network.py
@@ -1,4 +1,5 @@
 import numpy as np
+from LogisticRegression.LogisticRegression import sigmoid
 
 
 def onehot_encode(label):
@@ -14,3 +15,29 @@ def onehot_encode(label):
         y_matrix.append((label == cls_idx).astype(int))
     one_hot = np.array(y_matrix).T
     return one_hot, cls
+
+
+class ForwardModel:
+    def __init__(self):
+        self.theta1 = None
+        self.theta2 = None
+
+    def load_parameters(self, parameters):
+        self.theta1 = parameters[0]
+        self.theta2 = parameters[1]
+
+    def __call__(self, x, *args, **kwargs):
+        # x (n,d)
+        t = np.ones(shape=(x.shape[0], 1))
+        # x (n,d+1)
+        a1 = np.concatenate((t, x), axis=1)
+        # a2 （n, hidden_size）
+        a2 = sigmoid(np.matmul(a1, self.theta1))
+        # a2 （n, hidden_size + 1）
+        a2 = np.concatenate((t, a2), axis=1)
+        # a3 （n, cls_n）
+        a3 = sigmoid(np.matmul(a2, self.theta2))
+        return a3
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import numpy as np
+from NueralNeywork.Network import ForwardModel
 
-a = np.array([[0, 1],
-              [1, 4],
-              [2, 7],
-              [3, 10]])
-print(a.mean(axis=0))
-print(a.std(axis=0))
-a = (a - a.mean(axis=0)) / a.std()
-print(a)
-print(a[0,0] * 3 + 1)
+model = ForwardModel()
+model(1)
